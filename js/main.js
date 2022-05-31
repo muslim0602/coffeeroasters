ce46887bbfea71466d2450aa43a7a5051e3ac8ff
muslim0602 <muslimturaboyev0602@gmail.com>
2022-05-31 10:51:04 +0500

    add dark mode

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+const elDarkModeButton = document.querySelector('.dark-mode-btn');
+const elLightModeButton = document.querySelector('.light-mode-btn');
+
+elDarkModeButton.addEventListener('click', function () {
+  document.body.classList.add('dark-mode')
+});
+
+elLightModeButton.addEventListener(
+  'click', function name(params) {
+    document.body.classList.add('light-mode')
+  }
+);
